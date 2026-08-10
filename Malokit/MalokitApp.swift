a6ccdb08d2799dad0b4e9b2e253d4ad70495c738
anin <banindya10@gmail.com>
2026-08-10 10:15:24 +0700

    Initial Commit

---
+++ Malokit/MalokitApp.swift
@@ -0,0 +1,32 @@
+//
+//  MalokitApp.swift
+//  Malokit
+//
+//  Created by Benedikta Anin on 10/08/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MalokitApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ MalokitTests/MalokitTests.swift
@@ -0,0 +1,19 @@
+//
+//  MalokitTests.swift
+//  MalokitTests
+//
+//  Created by Benedikta Anin on 10/08/26.
+//
+
+import Testing
+@testable import Malokit
+
+struct MalokitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
